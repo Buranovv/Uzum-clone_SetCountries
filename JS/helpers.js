7cbe23169006481b3c5bd7e9602c642d9879f268
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-24 01:29:55 +0500

    sort done

---
+++ JS/helpers.js
@@ -0,0 +1,33 @@
+function getElement(element, parent = document) {
+  return parent.querySelector(element);
+}
+
+function renderFn(array, parent) {
+  parent.innerHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+    const newCard = document.createElement("div");
+    newCard.className = "card card-style";
+    newCard.style.width = "15rem";
+    newCard.style.width = "15rem";
+
+    newCard.innerHTML = `
+        <div class="img-box">
+           <img class="card-img-top" width="110"  src="${array[i].image}" alt="">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${array[i].title}</h5>
+          <div class="card-body-style">
+          <p class="card-rate">
+          <img data-v-b3e9397c="" src="data:image/svg+xml,%3csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3e %3cpath d='M9 12.9525L13.635 15.75L12.405 10.4775L16.5 6.93L11.1075 6.4725L9 1.5L6.8925 6.4725L1.5 6.93L5.595 10.4775L4.365 15.75L9 12.9525Z' fill='%23F5A623'/%3e %3c/svg%3e" class="noselect rating-icon" data-test-id="icon__rating-star" style="width: 14px;">
+          ${array[i].rating.rate}
+          </p>
+          <p class="dont">Bo'lib to'lash yo'q</p>
+          <p class="card-price">$${array[i].price}</p>
+          </div>
+        </div>
+        `;
+
+    parent.appendChild(newCard);
+  }
+}
--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,39 @@
-function getElement(element, parent = document) {
-  return parent.querySelector(element);
-}
-
 const elCards = getElement(".cards");
+const elElec = getElement("#electronics");
+const elJew = getElement("#jewelery");
+const elCloth = getElement("#clothing");
 
-function renderFn(array, parent) {
-  parent.innerHTML = "";
-
-  for (let i = 0; i < array.length; i++) {
-    const newCard = document.createElement("div");
-    newCard.className = "card card-style";
-    newCard.style.width = "15rem";
-    newCard.style.width = "15rem";
-
-    newCard.innerHTML = `
-      <div class="img-box">
-         <img class="card-img-top" width="110"  src="${array[i].image}" alt="">
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">${array[i].title}</h5>
-        <div class="card-body-style">
-        <p class="card-rate">
-        <img data-v-b3e9397c="" src="data:image/svg+xml,%3csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3e %3cpath d='M9 12.9525L13.635 15.75L12.405 10.4775L16.5 6.93L11.1075 6.4725L9 1.5L6.8925 6.4725L1.5 6.93L5.595 10.4775L4.365 15.75L9 12.9525Z' fill='%23F5A623'/%3e %3c/svg%3e" class="noselect rating-icon" data-test-id="icon__rating-star" style="width: 14px;">
-        ${array[i].rating.rate}
-        </p>
-        <p class="dont">Bo'lib to'lash yo'q</p>
-        <p class="card-price">$${array[i].price}</p>
-        </div>
-      </div>
-      `;
-
-    parent.appendChild(newCard);
-  }
-}
 renderFn(products, elCards);
+
+elElec.addEventListener("click", () => {
+  const elec = [];
+
+  products.forEach((element) => {
+    if (element.category == "electronics") {
+      elec.push(element);
+    }
+  });
+  renderFn(elec, elCards);
+});
+
+elJew.addEventListener("click", () => {
+  const jew = [];
+
+  products.forEach((element) => {
+    if (element.category == "jewelery") {
+      jew.push(element);
+    }
+  });
+  renderFn(jew, elCards);
+});
+
+elCloth.addEventListener("click", () => {
+  const cloth = [];
+
+  products.forEach((element) => {
+    if (element.category == "clothing") {
+      cloth.push(element);
+    }
+  });
+  renderFn(cloth, elCards);
+});
