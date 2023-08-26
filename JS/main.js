0fd0cd980ce378f7f0b4f8fc766ba561ae17c0d7
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-26 20:30:06 +0500

    start

--- JS/main.js
+++ JS/main.js
@@ -1,66 +1,7 @@
 const elCards = getElement(".cards");
-const elElec = getElement("#electronics");
-const elJew = getElement("#jewelery");
-const elCloth = getElement("#clothing");
-const elForm = getElement("#form");
-const elSearch = getElement("#search-input");
-const elSearchBtn = getElement("#search-btn");
-
-renderFn(products, elCards);
-
-elElec.addEventListener("click", () => {
-  const elec = [];
-
-  products.forEach((element) => {
-    if (element.category == "electronics") {
-      elec.push(element);
-    }
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+    renderFn(res, elCards);
   });
-  renderFn(elec, elCards);
-});
-
-elJew.addEventListener("click", () => {
-  const jew = [];
-
-  products.forEach((element) => {
-    if (element.category == "jewelery") {
-      jew.push(element);
-    }
-  });
-  renderFn(jew, elCards);
-});
-
-elCloth.addEventListener("click", () => {
-  const cloth = [];
-
-  products.forEach((element) => {
-    if (element.category == "clothing") {
-      cloth.push(element);
-    }
-  });
-  renderFn(cloth, elCards);
-});
-
-elCards.addEventListener("click", (evt) => {
-  if (evt.target.id === "ui-icon-f" || evt.target.id === "ui-icon") {
-    const id = Number(evt.target.dataset.id);
-    products.forEach((element) => {
-      if (element.id === id) {
-        element.isFav = !element.isFav;
-      }
-    });
-  }
-  renderFn(products, elCards);
-});
-
-elForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const newArr = [];
-  products.forEach((element) => {
-    if (element.title.toLowerCase().includes(elSearch.value.toLowerCase())) {
-      newArr.push(element);
-    }
-  });
-  elForm.reset();
-  renderFn(newArr, elCards);
-});
