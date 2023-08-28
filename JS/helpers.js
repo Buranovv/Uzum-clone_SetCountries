599bc626bb0d5dead2f60db7891734046fcb0286
Buranovv <zahiriddinburanov007@gmail.com>
2023-08-29 01:48:31 +0500

    done

--- JS/helpers.js
+++ JS/helpers.js
@@ -6,20 +6,21 @@ function renderFn(array, parent) {
   parent.innerHTML = "";
 
   for (let i = 0; i < array.length; i++) {
+    const element = array[i]
     const newCard = document.createElement("div");
     newCard.className = "card card-style";
     newCard.style.width = "18rem";
 
     newCard.innerHTML = `
         <div class="img-box">
-           <img class="card-img-top" width="110"  src="${array[i].flags.png}" alt="">
+           <img class="card-img-top" width="110"  src="${element.flags.png}" alt="">
         </div>
         <div class="card-body">
-          <h5 class="card-title">${array[i].name.official}</h5>
+          <h5 class="card-title">${element.name.official}</h5>
           <div class="card-body-style">
-          <p class="card-population">Population: <span class="population-style">${array[i].population}</span></p>
-          <p class="card-region">Region: <span class="region-style">${array[i].region}</span></p>
-          <p class="card-capital">Capital: <span class="capital-style">${array[i].capital}</span></p>
+          <p class="card-population">Population: <span class="population-style">${element.population}</span></p>
+          <p class="card-region">Region: <span class="region-style">${element.region}</span></p>
+          <p class="card-capital">Capital: <span class="capital-style">${element.capital}</span></p>
           </div>
         </div>
         `;
--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,46 @@
 const elCards = getElement(".cards");
+const elForm = getElement("#form");
+const elSearch = getElement(".search");
+const elFilterOpen = getElement(".filter__title");
+const elFilterClose = getElement(".filter__close");
+const africa = getElement(".africa");
+const america = getElement(".america");
+const asia = getElement(".asia");
+const europe = getElement(".europe");
+const ocean = getElement(".ocean");
+
+let countries;
+
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((res) => {
     console.log(res);
     renderFn(res, elCards);
+    countries = res;
   });
+
+elForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const newArr = [];
+
+  countries.forEach((element) => {
+    if (element.name.official.includes(elSearch.value)) {
+      newArr.push(element);
+    }
+  });
+  elForm.reset();
+  renderFn(newArr, elCards);
+});
+
+elFilterOpen.addEventListener("click", () => {
+  elFilterList.className += " show";
+  elFilterClose.className += " show";
+  elFilterOpen.className += " close";
+});
+
+elFilterClose.addEventListener("click", () => {
+  elFilterOpen.className = "filter__title";
+  elFilterList.className = "filter__list";
+  elFilterClose.className = "filter__close";
+});
+
