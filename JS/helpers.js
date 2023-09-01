441c4ca7cdee48b851152b3f217fc772256dff3f
Buranovv <zahiriddinburanov007@gmail.com>
2023-09-01 22:44:06 +0500

    fix

--- JS/helpers.js
+++ JS/helpers.js
@@ -1,8 +1,8 @@
-function getElement(element, parent = document) {
+export function getElement(element, parent = document) {
   return parent.querySelector(element);
 }
 
-function renderFn(array, parent) {
+export function renderFn(array, parent) {
   parent.innerHTML = "";
 
   for (let i = 0; i < array.length; i++) {
@@ -46,10 +46,18 @@ function renderFn(array, parent) {
           <div class="card-body-style">
           <p class="card-rate">
           <img data-v-b3e9397c="" src="data:image/svg+xml,%3csvg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'%3e %3cpath d='M9 12.9525L13.635 15.75L12.405 10.4775L16.5 6.93L11.1075 6.4725L9 1.5L6.8925 6.4725L1.5 6.93L5.595 10.4775L4.365 15.75L9 12.9525Z' fill='%23F5A623'/%3e %3c/svg%3e" class="noselect rating-icon" data-test-id="icon__rating-star" style="width: 14px;">
-          ${array[i].rating.rate}
+          ${array[i].createdAt}
           </p>
           <p class="dont">Bo'lib to'lash yo'q</p>
           <p class="card-price">$${array[i].price}</p>
+          <div class="btn-box d-flex align-items-center justify-content-center gap-2">
+          <button data-id="${
+            array[i].id
+          }" class="btn btn-danger w-100 d-none" id="delete-btn">Delete</button>
+          <button data-id="${
+            array[i].id
+          }" class="btn btn-primary w-100 d-none" id="edit-btn">Edit</button>
+          </div>
           </div>
         </div>
         `;
--- JS/main.js
+++ JS/main.js
@@ -1,66 +1,85 @@
+import { renderFn, getElement } from "./helpers.js";
+
 const elCards = getElement(".cards");
 const elElec = getElement("#electronics");
 const elJew = getElement("#jewelery");
 const elCloth = getElement("#clothing");
 const elForm = getElement("#form");
 const elSearch = getElement("#search-input");
-const elSearchBtn = getElement("#search-btn");
+const BASE_API = "https://64f17d800e1e60602d23d7d6.mockapi.io/";
 
-renderFn(products, elCards);
-
-elElec.addEventListener("click", () => {
-  const elec = [];
-
-  products.forEach((element) => {
-    if (element.category == "electronics") {
-      elec.push(element);
-    }
+fetch(BASE_API + "product")
+  .then((res) => res.json())
+  .then((res) => {
+    renderFn(res, elCards);
   });
-  renderFn(elec, elCards);
-});
 
-elJew.addEventListener("click", () => {
-  const jew = [];
+// elElec.addEventListener("click", () => {
+//   const elec = [];
 
-  products.forEach((element) => {
-    if (element.category == "jewelery") {
-      jew.push(element);
-    }
-  });
-  renderFn(jew, elCards);
-});
+//   products.forEach((element) => {
+//     if (element.category == "electronics") {
+//       elec.push(element);
+//     }
+//   });
+//   renderFn(elec, elCards);
+// });
 
-elCloth.addEventListener("click", () => {
-  const cloth = [];
+// elJew.addEventListener("click", () => {
+//   const jew = [];
 
-  products.forEach((element) => {
-    if (element.category == "clothing") {
-      cloth.push(element);
-    }
-  });
-  renderFn(cloth, elCards);
-});
+//   products.forEach((element) => {
+//     if (element.category == "jewelery") {
+//       jew.push(element);
+//     }
+//   });
+//   renderFn(jew, elCards);
+// });
+
+// elCloth.addEventListener("click", () => {
+//   const cloth = [];
+
+//   products.forEach((element) => {
+//     if (element.category == "clothing") {
+//       cloth.push(element);
+//     }
+//   });
+//   renderFn(cloth, elCards);
+// });
 
 elCards.addEventListener("click", (evt) => {
-  if (evt.target.id === "ui-icon-f" || evt.target.id === "ui-icon") {
-    const id = Number(evt.target.dataset.id);
-    products.forEach((element) => {
-      if (element.id === id) {
-        element.isFav = !element.isFav;
+  fetch(BASE_API + "product")
+    .then((res) => res.json())
+    .then((res) => {
+      if (evt.target.id === "ui-icon-f" || evt.target.id === "ui-icon") {
+        const id = Number(evt.target.dataset.id);
+        res.forEach((element) => {
+          if (element.id === id) {
+            element.isFav = !element.isFav;
+          }
+        });
       }
+      renderFn(res, elCards);
     });
-  }
-  renderFn(products, elCards);
 });
 
 elForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  const newArr = [];
-  products.forEach((element) => {
-    if (element.title.toLowerCase().includes(elSearch.value.toLowerCase())) {
-      newArr.push(element);
-    }
-  });
-  elForm.reset();
-  renderFn(newArr, elCards);
+  fetch(BASE_API + "product")
+    .then((res) => res.json())
+    .then((res) => {
+      const newArr = [];
+
+      res.forEach((element) => {
+        if (
+          element.title.toLowerCase().includes(elSearch.value.toLowerCase())
+        ) {
+          newArr.push(element);
+        }
+        renderFn(newArr, elCards);
+      });
+      elForm.reset();
+      renderFn(newArr, elCards);
+    });
 });
+
